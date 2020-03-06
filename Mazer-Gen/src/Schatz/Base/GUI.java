80c63e59b5957fabc5bf2d225afb33b01b3fa203
radfre <39837614+radfre@users.noreply.github.com>
2020-03-06 09:09:29 -0600

    updates program

---
+++ Mazer-Gen/src/Schatz/Base/GUI.java
@@ -0,0 +1,86 @@
+package Base;
+
+
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame {
+    private static JFrame frame;
+    public static Boolean Done;
+    public static String End;
+
+    private JPanel Panel0;
+    private JSlider WidthSlider;
+    private JSlider HeightSlider;
+    private JSlider SizeSlider;
+    private JButton DoneButton;
+    private JComboBox MazeLib;
+    private JLabel WidthText;
+    private JLabel HeightText;
+    private JLabel SizeText;
+    private JLabel cellLable;
+    private JLabel heightLabel;
+    private JLabel widthLabel;
+    private JCheckBox watchCheckBox;
+
+    public GUI() {
+        WidthSlider.addChangeListener(new ChangeListener() {
+                                          @Override
+                                          public void stateChanged(ChangeEvent changeEvent) {
+                                              WidthText.setText("" + WidthSlider.getValue());
+                                          }
+                                      }
+        );
+        HeightSlider.addChangeListener(new ChangeListener() {
+                                           @Override
+                                           public void stateChanged(ChangeEvent changeEvent) {
+                                               HeightText.setText("" + HeightSlider.getValue());
+                                           }
+                                       }
+        );
+        SizeSlider.addChangeListener(new ChangeListener() {
+                                         @Override
+                                         public void stateChanged(ChangeEvent changeEvent) {
+                                             SizeText.setText("" + SizeSlider.getValue());
+                                             HeightSlider.setMaximum(900/SizeSlider.getValue());
+                                             WidthSlider.setMaximum(1750/SizeSlider.getValue());
+                                         }
+                                     }
+        );
+
+
+        DoneButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                frame.setVisible(false);
+                End += MazeLib.getSelectedItem() + " ";
+                End += WidthText.getText() + " ";
+                End += HeightText.getText() + " ";
+                End += SizeText.getText() + " ";
+                End += (Boolean) watchCheckBox.isSelected();
+
+                Done = true;
+
+            }
+        });
+    }
+
+    public String getComboName(int i) {
+        return "" + MazeLib.getItemAt(i);
+    }
+
+    public static void main(String[] args) {
+        End = "";
+        Done = false;
+        frame = new JFrame("GUI");
+        frame.setContentPane(new GUI().Panel0);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ Mazer-Gen/src/Schatz/Mazes/Maze.java
@@ -0,0 +1,160 @@
+package Mazes;
+
+import Mazes.Cell.*;
+import Mazes.Cell.Cell;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public abstract class Maze {
+    protected static int visited;
+    protected static int total;
+    protected int width;
+    protected int height;
+    public int offsetx;
+    public int offsety;
+    protected Cell[][] cells;
+    public int Maze, size;
+    public Boolean Done = false;
+    protected Random r = new Random();
+    protected int x;
+    protected int y;
+    protected boolean watch;
+    protected static ArrayList<Boolean> open;
+
+
+    public Maze(int width, int height, int size, int offsetx, int offsety,boolean watch) {
+        this.width = width;
+        this.height = height;
+        cells = new Cell[width][height];
+        this.Maze = Maze;
+        this.size = size;
+        this.offsetx = offsetx;
+        this.offsety = offsety;
+        Done=watch;
+        this.watch=watch;
+
+        visited = 1;
+        total = width * height;
+
+    }
+
+    public void Draw(Graphics g, Color color0, Color color1){
+        g.setColor(color1);
+        g.fillRect(0, 0, width * size + offsetx * 2, height * size + offsety * 2);
+
+        g.setColor(color0);
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                cells[i][j].draw(g,color0,color1);
+
+            }
+        }
+
+    }
+
+
+    public abstract void work();
+
+
+
+    // Move
+
+    protected boolean isOpen(ArrayList<Boolean> open) {
+        for (int i = 0; i < open.size(); i++) {
+            if (open.get(i))
+                return true;
+        }
+        return false;
+    }
+
+
+    // 0-down,1-left,2-up,3-right
+    protected ArrayList<Boolean> openings() {
+        ArrayList<Boolean> open = new ArrayList<Boolean>(4);
+
+        if (y + 2 > height || cells[x][y + 1].visited) {
+            open.add(false);
+        } else
+            open.add(true);
+
+        if (x - 1 < 0 || cells[x - 1][y].visited) {
+            open.add(false);
+        } else
+            open.add(true);
+
+        if (y - 1 < 0 || cells[x][y - 1].visited) {
+            open.add(false);
+        } else
+            open.add(true);
+
+        if (x + 2 > width || cells[x + 1][y].visited) {
+            open.add(false);
+        } else
+            open.add(true);
+
+        return open;
+
+    }
+
+    protected void Left() {
+        x--;
+        cells[x][y].visit();
+        delRight(x, y);
+    }
+
+    protected void Right() {
+        x++;
+        cells[x][y].visit();
+        delLeft(x, y);
+    }
+
+    protected void Up() {
+        y--;
+        cells[x][y].visit();
+        delDown(x, y);
+    }
+
+    protected void Down() {
+        y++;
+        cells[x][y].visit();
+        delUp(x, y);
+    }
+
+    // 0-down,1-left,2-up,3-right
+    protected void delUp(int x, int y) {
+        cells[x][y].eraseUp();
+        if (y - 1 >= 0)
+            cells[x][y - 1].eraseDown();
+
+    }
+
+    protected void delDown(int x, int y) {
+        cells[x][y].eraseDown();
+        if (y + 1 < height)
+            cells[x][y + 1].eraseUp();
+    }
+
+    protected void delRight(int x, int y) {
+        cells[x][y].eraseRight();
+        if (x + 1 < width)
+            cells[x + 1][y].eraseLeft();
+    }
+
+    protected void delLeft(int x, int y) {
+        cells[x][y].eraseLeft();
+        if (x - 1 >= 0)
+            cells[x - 1][y].eraseRight();
+
+    }
+
+    protected void Wait() throws InterruptedException {
+        Thread.sleep(5);
+
+    }
+
+
+}
